7a933fd970ec35197a3d577a53bfeaf0da5b02d6
costal25 <92455142+costal25@users.noreply.github.com>
2022-06-10 12:39:32 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//index order form
+
+function showOrder ()
+{
+    document.getElementById('order').style.display='block'; 
+
+    document.getElementById("button").style.display='none';
+}
+
+function hideOrder() 
+{
+    document.getElementById('order').style.display='none'; 
+
+    document.getElementById("button").style.display='block';
+}
+
